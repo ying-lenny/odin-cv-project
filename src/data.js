bca64082228499dafa59cb5cf375cb20067e2729
Owen Lenihan <owenlenihan101@gmail.com>
2022-12-13 22:17:18 +0000

    Create data.js

---
+++ src/data.js
@@ -0,0 +1,83 @@
+let data = {
+    name: { first: 'Some', last: 'Name' },
+    contact: {
+        phone: '000-000-000',
+        email: 'email@email.com',
+        linkedIn: 'linkedinprofile',
+    },
+    social: [
+        {
+            id: 0,
+            website: 'Instagram',
+            userName: 'insta_gram_',
+        },
+        {
+            id: 1,
+            website: 'Facebook',
+            userName: 'facebook',
+        },
+    ],
+    interests: ['things', 'you', 'like', 'doing'],
+    summary:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    career: [
+        {
+            id: 0,
+            year: '2010 - 2020',
+            company: 'Company 1',
+            position: 'Position 1',
+            workDone:
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        },
+        {
+            id: 1,
+            year: '2008 - 2010',
+            company: 'Company 2',
+            position: 'Position 2',
+            workDone:
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        },
+    ],
+    education: [
+        {
+            id: 0,
+            year: '1996 - 2004',
+            school: 'School',
+            title: 'School',
+        },
+        {
+            id: 1,
+            year: '2004 - 2008',
+            school: 'Some Good College',
+            title: 'Bachelors in Something',
+        },
+    ],
+    skills: [
+        {
+            id: 0,
+            name: 'Skill 1',
+            rating: 3,
+        },
+        {
+            id: 1,
+            name: 'Skill 2',
+            rating: 5,
+        },
+        {
+            id: 2,
+            name: 'Skill 3',
+            rating: 4,
+        },
+        {
+            id: 3,
+            name: 'Skill 4',
+            rating: 4,
+        },
+        {
+            id: 4,
+            name: 'Skill 5',
+            rating: 5,
+        },
+    ],
+};
+export default data;
